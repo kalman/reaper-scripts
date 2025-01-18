51baf8460fe979af80f55d09b413ff3d742780e7
Benjamin Kalman <btkalman@gmail.com>
2025-01-18 13:05:44 +1100

    rename extend to shrink

--- ShrinkTimeSelectionLeft.lua
+++ ShrinkTimeSelectionLeft.lua
@@ -4,7 +4,7 @@ package.path = folder_path .. '?.lua;'
 local btk = require 'btk'
 local rpr = require 'rpr'
 
-btk.main("ExtendTimeSelectionRight", function()
+btk.main("ShrinkTimeSelectionRight", function()
     local loopStart, loopEnd = btk.GetLoopTimeRangeOrCursor()
     btk.SetLoopTimeRange(loopStart, math.max(loopStart, loopEnd - 1))
 end)
